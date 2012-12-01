14f86d1dcfdc76157ce7c48ac7073bb72c624097
Botond Kis <boti.kis@gmx.at>
2012-12-01 22:21:30 +0100

    added path style sample project

---
+++ PathSample/PathSample/PathSample/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  PathSample
+//
+//  Created by Botond Kis on 01.12.12.
+//  Copyright (c) 2012 aaa - AllAboutApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ PathSample/PathSample/PathSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PathSample
+//
+//  Created by Botond Kis on 01.12.12.
+//  Copyright (c) 2012 aaa - AllAboutApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
